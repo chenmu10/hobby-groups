638dc8843dedcdf81985939c47b2abda3f169ddf
Chen Mualem <142717480+chenmualem@users.noreply.github.com>
2025-02-14 23:54:28 +0200

    Refactor database configuration and imports
    This commit refactors the database configuration and imports in the `db/drizzle.ts` file. updates the `drizzle` import to use `drizzle-orm/neon-http`. Additionally, it updates the export of the `db` object to use the new `neon` instance and removes the unused `Pool` import.
    
    Also, it updates the `schema` path in the `drizzle.config.ts` file to point to the correct location in the project.

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -1,13 +1,8 @@
 import { config } from "dotenv";
-import { Pool } from '@neondatabase/serverless';
-
-import { drizzle } from 'drizzle-orm/neon-serverless';
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
 
 config({ path: ".env" }); // or .env.local
 
-const pool = new Pool({connectionString: process.env.DATABASE_URL! });
-
-export const db = drizzle(pool);
-
-
-
+export const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle(sql);
